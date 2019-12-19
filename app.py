2c9d7cc4af0e4ad54745e68f0d493f2084be6a40
rphanley <rphanley@rocketmail.com>
2019-12-19 17:42:24 +0000

    Cleaned up code per pylint with REST paths. Moved util out,added docstrings and flash messages

--- app.py
+++ app.py
@@ -1,137 +1,109 @@
+"""App to handle CRUD operations for the energy_glossary terms database"""
 import os
 import re
-import datetime
-
-
-
 
 from dotenv import load_dotenv
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, flash, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
-
+from utils import created_today
 
 load_dotenv(override=True)
-app = Flask(__name__)
-app.config['MONGO_DBNAME'] = 'energy_glossary'
-app.config['MONGO_URI'] = os.getenv('MONGO_URI', 'Env value not loaded')
+APP = Flask(__name__)
+APP.config['MONGO_DBNAME'] = 'energy_glossary'
+APP.config['MONGO_URI'] = os.getenv('MONGO_URI', 'Env value not loaded')
+APP.secret_key = os.getenv('SECRET_KEY')
 
-mongo = PyMongo(app)
+MONGO = PyMongo(APP)
 
-
-@app.route('/')
-@app.route('/home')
+@APP.route('/')
 def home():
-    latest = mongo.db.terms.find().sort('_id',-1).limit(10)
+    """Home page with search input and display of the most recent database entries."""
+    latest = MONGO.db.terms.find().sort('_id', -1).limit(10)
     return render_template("index.html", records=latest)
 
 
-
-
-@app.route('/search_terms', methods=['POST'])
+@APP.route('/search_terms', methods=['POST'])
 def search_terms():
+    """Posts the search for the requested term and handles the single returned record, or any errors"""
     result = None
-    
     #Get the search text from the input box
     search_text = request.form['search_input']
     #Run a case-insensitive search for a record with term=search_text
     try:
-        result = mongo.db.terms.find_one({'term': re.compile(search_text, re.IGNORECASE)})
-       
+        result = MONGO.db.terms.find_one({'term': re.compile(search_text, re.IGNORECASE)})
     except:
         # Notify the user
-        msg = 'Error accessing the database! Check your database access and try again.'
-        return render_template("message.html",
-                                message_text=msg)
-
-    
+        flash('Error accessing the database! Check your database access and try again.')
+        return redirect(url_for('home'))
     # Print an error if no result found, otherwise get the result record.
     if not result:
-        msg = 'No result found for: ' + search_text + ' .Check your spelling and try again.'
-        return render_template("message.html",
-                                message_text=msg)
+        flash('No result found for: ' + '"' + search_text + '"' + ' .Check your spelling and try again.')
+        return redirect(url_for('home'))
     else:
         print("Found a database entry for: " + search_text)
         term_id = result['_id']
         return redirect(url_for('get_record', record_id=term_id))
 
 
-@app.route('/get_record/<record_id>')
+@APP.route('/terms/<record_id>')
 def get_record(record_id):
+    """Gets the single found record from the database, checks if it was created today, and passes it for display"""
     badge = ''
-    print('Getting record ID ' + record_id)
-    record = mongo.db.terms.find_one({'_id': ObjectId(record_id)})
-    print('Got record ID with term: ' + record['term'])
+    record = MONGO.db.terms.find_one({'_id': ObjectId(record_id)})
     if created_today(record_id):
         badge = 'new badge'
-    return render_template("read.html",
-                                result_term=record, badge_class=badge)
+    return render_template("read.html", result_term=record, badge_class=badge)
 
-
-# Check if the database entry was created today, for New badge
-def created_today(entry_id):
-    obj_id = ObjectId(entry_id)
-    create_date = obj_id.generation_time.date()
-    print(create_date)
-    today = datetime.datetime.utcnow().date()
-    print(today)
-    return (create_date == today)
-
-@app.route('/add')
+@APP.route('/terms/new', methods=['GET', 'POST'])
 def add():
-    return render_template('add.html')
-
-
-
-@app.route('/update/<term_id>')
-def update(term_id):
-    term = mongo.db.terms.find_one({"_id": ObjectId(term_id)})
-    return render_template('update.html', term_to_update=term)
-
-
-
-#Perform the record insertion in the database
-@app.route('/add_entry', methods=['POST'])
-def add_entry():
-    terms =  mongo.db.terms
-    # Add a new record
-    terms.insert_one({
-        'term':request.form.get('term_name'),
-        'description':request.form.get('term_description'),
-        })
-    print('Added record..')
-    return redirect(url_for('home'))
+    """Gets or posts the add term form to insert a new entry in the database."""
+    if request.method == 'POST':
+        terms = MONGO.db.terms
+        # Add a new record
+        new_term = request.form.get('term_name')
+        terms.insert_one({
+            'term':new_term,
+            'description':request.form.get('term_description'),
+            })
+        result = terms.find_one({'term': re.compile(new_term, re.IGNORECASE)})
+        flash('Entry for ' + '"' + new_term + '"' + ' created!')
+        return render_template("read.html", result_term=result, badge_class='new badge')
+    else:
+        return render_template('add.html')
 
 #Perform the record update in the database
-@app.route('/update_entry/<term_id>', methods=['POST'])
-def update_entry(term_id):
-    terms =  mongo.db.terms
+@APP.route('/terms/<term_id>/edit', methods=['GET', 'POST'])
+def update(term_id):
+    """Gets or posts the update term form to update an existing entry in the database."""
+    if request.method == 'POST':
+        terms = MONGO.db.terms
+        badge = ''
+        #Update the record
+        terms.update({'_id': ObjectId(term_id)},
+                     {'term':request.form.get('term_name'),
+                      'description':request.form.get('term_description'),})
+        record = terms.find_one({'_id': ObjectId(term_id)})
+        if created_today(term_id):
+            badge = 'new badge'
+        flash('Entry for ' + '"' + record['term'] + '"' + ' updated!')
+        return render_template("read.html", result_term=record, badge_class=badge)
+    else:
+        term = MONGO.db.terms.find_one({"_id": ObjectId(term_id)})
+        return render_template('update.html', term_to_update=term)
 
-    #Update the record
-    terms.update( {'_id': ObjectId(term_id)},
-        {
-        'term':request.form.get('term_name'),
-        'description':request.form.get('term_description'),
-        })
-    print('Updated record..')
+
+@APP.route('/terms/<term_id>/')
+def delete(term_id):
+    """Deletes the given record from the database."""
+    entry = MONGO.db.terms.find_one({"_id": ObjectId(term_id)})
+    name = entry['term']
+    MONGO.db.terms.delete_one({'_id': ObjectId(term_id)})
+    # Notify the user
+    flash('Entry for ' + '"' + name + '"' + ' deleted!')
     return redirect(url_for('home'))
 
-
-@app.route('/delete/<term_id>')
-def delete(term_id):
-    entry = mongo.db.terms.find_one({"_id": ObjectId(term_id)})
-    name = entry['term']
-    mongo.db.terms.delete_one({'_id': ObjectId(term_id)})
-    # Notify the user
-    msg = 'Entry for ' + '"' + name + '"' + ' deleted!'
-
-    return render_template("message.html",
-                                message_text=msg)
-         
-    
-
-
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP','0.0.0.0'),
-            port=int(os.environ.get('PORT','5000')),
-            debug=True)
+    APP.run(host=os.environ.get('IP', '0.0.0.0'),
+            port=int(os.environ.get('PORT', '5000')),
+            debug=False)
